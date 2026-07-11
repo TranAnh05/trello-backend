ddef2b00a3a3096e94c49585dd1d18261561dc03
TranAnh05 <anh900472@gmail.com>
2026-07-11 22:17:44 +0700

    Api update the order of column

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -31,6 +31,9 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+// Chi dinh nhung fields ma chung ta khong muon cap nhat
+const INVALID_UPDATE_FIELDS = ['_id', 'createdAt']
+
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
     abortEarly: false
@@ -109,7 +112,30 @@ const pushColumnOrderIds = async (column) => {
         { returnDocument: 'after' }
       )
 
-    return result.value
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (boardId, updateData) => {
+  try {
+    // Loc nhung fields khong cho phep update
+    Object.keys(updateData).forEach(fieldName => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+
+    const result = await getDatabaseInstance()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOneAndUpdate(
+        { _id: new ObjectId(boardId) },
+        { $set: updateData },
+        { returnDocument: 'after' }
+      )
+
+    return result
   } catch (error) {
     throw new Error(error)
   }
@@ -121,5 +147,6 @@ export const boardModel = {
   createNew,
   findOneById,
   getDetails,
-  pushColumnOrderIds
+  pushColumnOrderIds,
+  update
 }
