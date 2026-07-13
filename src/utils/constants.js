568c01271f9e1c1d21ab18bd36a7b8e09aff84fc
TranAnh05 <anh900472@gmail.com>
2026-07-13 11:45:57 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 // Nhung domain duoc phep truy cap vao tai nguyen cua server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
-  // domain sau nay se deploy
 ]
 
 export const BOARD_TYPES = {
